4f219872ca4f8b665778e3aca53c81f72a9687a1
DeveloperFromIndia <gnchrv.dll@gmail.com>
2025-12-05 17:10:20 +0100

    fix telegram locale shit

--- src/middleware/locale.ts
+++ src/middleware/locale.ts
@@ -1,10 +1,9 @@
 import { DEFAULT_LOCALE, type ConfigContext } from "i18n/config";
 
-const localeWhitelist = ["en", "ru", "ua"];
+const localeWhitelist = ["en", "ru", "uk"];
 
 const localeMiddleware = async (ctx: ConfigContext, next: () => Promise<void>) => {
     const userLocale = ctx.from?.language_code ?? DEFAULT_LOCALE;
-    
     if (localeWhitelist.includes(userLocale)) {
         ctx.i18n.useLocale(userLocale);
     } else {
